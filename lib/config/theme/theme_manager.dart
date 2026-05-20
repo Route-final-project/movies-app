e72e3d80da389032c6e0bf13d838ee467e4ef02a
omer1998 <omerfaris11@gmail.com>
2026-05-20 23:37:57 +0300

    Update ThemeManager to define comprehensive appTheme with updated typography and component styles

--- lib/config/theme/theme_manager.dart
+++ lib/config/theme/theme_manager.dart
@@ -4,22 +4,99 @@ import 'package:google_fonts/google_fonts.dart';
 
 import '../../core/resource/colors_manager.dart';
 
-abstract class ThemeManager{
-  static final ThemeData light = ThemeData(
+abstract class ThemeManager {
+  static final ThemeData appTheme = ThemeData(
     appBarTheme: AppBarTheme(
-      iconTheme: IconThemeData(color: ColorsManager.white),
+      iconTheme: IconThemeData(color: ColorsManager.gold),
       backgroundColor: ColorsManager.black,
-      foregroundColor: ColorsManager.white,
-      titleTextStyle:GoogleFonts.inter(color: ColorsManager.white, fontSize: 20.sp,fontWeight: FontWeight.w500 ),
-centerTitle: true,
+      foregroundColor: ColorsManager.gold,
+      titleTextStyle: GoogleFonts.roboto(
+        color: ColorsManager.white,
+        fontSize: 16.sp,
+        fontWeight: FontWeight.w400,
+      ),
+      centerTitle: true,
     ),
     scaffoldBackgroundColor: ColorsManager.black,
- drawerTheme: DrawerThemeData(
-   backgroundColor: ColorsManager.black
- ),
     textTheme: TextTheme(
-      titleMedium: GoogleFonts.inter(fontSize: 24.sp, fontWeight: FontWeight.bold, color: ColorsManager.white)
+      titleLarge: GoogleFonts.roboto(
+        fontSize: 36.sp,
+        fontWeight: FontWeight.w500,
+        color: ColorsManager.white,
+      ),
+      titleMedium: GoogleFonts.roboto(
+        fontSize: 24.sp,
+        fontWeight: FontWeight.w700,
+        color: ColorsManager.white,
+      ),
+      titleSmall: GoogleFonts.roboto(
+        fontSize: 20.sp,
+        fontWeight: FontWeight.w700,
+        color: ColorsManager.white,
+      ),
+      bodyMedium: GoogleFonts.roboto(
+        fontSize: 20.sp,
+        fontWeight: FontWeight.w400,
+        color: ColorsManager.white,
+      ),
+      bodySmall: GoogleFonts.roboto(
+        fontSize: 16.sp,
+        fontWeight: FontWeight.w400,
+        color: ColorsManager.white,
+      ),
+    ),
+
+    inputDecorationTheme: InputDecorationTheme(
+      hintStyle: GoogleFonts.roboto(
+        fontSize: 16.sp,
+        fontWeight: FontWeight.w400,
+        color: ColorsManager.white,
+      ),
+      prefixIconColor: ColorsManager.white,
+      suffixIconColor: ColorsManager.white,
+      iconColor: ColorsManager.white,
+      filled: true,
+      fillColor: ColorsManager.grey,
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(15.r),
+        borderSide: BorderSide(color: ColorsManager.grey),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorsManager.red),
+        borderRadius: BorderRadius.circular(15.r),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorsManager.red),
+        borderRadius: BorderRadius.circular(15.r),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(15.r),
+        borderSide: BorderSide(color: ColorsManager.grey),
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(15.r),
+        borderSide: BorderSide(color: ColorsManager.grey),
+      ),
+    ),
+    iconTheme: IconThemeData(
+      color: ColorsManager.white,
+      size: 26.sp,
+    ),
+    filledButtonTheme: FilledButtonThemeData(
+      style: FilledButton.styleFrom(
+        backgroundColor: ColorsManager.gold,
+        foregroundColor: ColorsManager.black,
+        iconColor: ColorsManager.black,
+        iconSize: 26.sp,
+        padding: EdgeInsets.all(8.sp),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.r),
+        ),
+        textStyle: GoogleFonts.roboto(
+          fontSize: 20.sp,
+          fontWeight: FontWeight.w400,
+        ),
+      )
     )
   );
-  static final ThemeData dark = ThemeData();
 }
