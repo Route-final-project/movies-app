92955a8ba625064af69413fd05fcc78034c6455b
omer1998 <omerfaris11@gmail.com>
2026-05-22 21:09:30 +0300

    Add controller, focusNode, and onTap properties to AppTextField

--- lib/presentation/common_component/appTextField.dart
+++ lib/presentation/common_component/appTextField.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -6,7 +5,10 @@ import '../../core/resource/colors_manager.dart';
 
 class AppTextField extends StatelessWidget {
   AppTextField({
+    this.onTap,
+    this.focusNode,
     this.suffixIcon,
+    this.controller,
     required this.prefixIcon,
     this.obscureText = false,
     required this.hintText,
@@ -19,6 +21,8 @@ class AppTextField extends StatelessWidget {
     super.key,
   });
 
+  final TextEditingController? controller;
+
   final Widget? suffixIcon;
   final void Function()? onSuffixClicked;
   final Widget prefixIcon;
@@ -28,11 +32,16 @@ class AppTextField extends StatelessWidget {
   final TextInputType keyboardType;
   final TextInputAction textInputAction;
   final int maxLines;
+  final FocusNode? focusNode;
   final void Function(String value)? onChanged;
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
+      focusNode: focusNode,
+      onTap: onTap,
       onChanged: onChanged,
       maxLines: maxLines,
       obscureText: obscureText,
@@ -48,13 +57,12 @@ class AppTextField extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: prefixIcon,
         ),
+        prefixIconConstraints: BoxConstraints(minWidth: 20.w, minHeight: 20.h),
         suffixIcon: suffixIcon != null
             ? Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: InkWell(
-              onTap: onSuffixClicked,
-              child: suffixIcon),
-        )
+                padding: const EdgeInsets.all(8.0),
+                child: InkWell(onTap: onSuffixClicked, child: suffixIcon),
+              )
             : null,
       ),
     );
