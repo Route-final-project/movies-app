c52857befef2afaef5c501f339a23e85df971544
omer1998 <omerfaris11@gmail.com>
2026-05-22 21:09:40 +0300

    Update assets_manager.dart to include search icon and empty search image resources

--- lib/core/resource/assets_manager.dart
+++ lib/core/resource/assets_manager.dart
@@ -1,5 +1,13 @@
 abstract class ImageAssets{
-  static const String _base = "assets/images/";
-  static const String availableNowImage = "${_base}available_now_image.png";
-  static const String watchNowImage = "${_base}watch_now_image.png";
+  static const String _baseImages = "assets/images/";
+  static const String availableNowImage = "${_baseImages}available_now_image.png";
+  static const String watchNowImage = "${_baseImages}watch_now_image.png";
+  static const String emptySearchImage = "${_baseImages}empty_search_image.png";
+
+}
+
+abstract class IconAssets{
+  static const String _baseIcons = "assets/icons/";
+  static const String searchIcon = "${_baseIcons}search_icon.svg";
+
 }
