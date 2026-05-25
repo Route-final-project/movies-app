83dc270dfe1142b814dac32041fe0c7a096aa85b
omer1998 <omerfaris11@gmail.com>
2026-05-26 00:14:37 +0300

    Add movie details and similar movies endpoints to app_constants.dart

--- lib/config/app_constants.dart
+++ lib/config/app_constants.dart
@@ -2,4 +2,7 @@ class AppConstants {
 
   static const String baseUrl = "https://movies-api.accel.li/api/v2/";
   static const String moviesListEndpoint = "list_movies.json";
+  static const String movieDetailsEndpoint = "movie_details.json";
+  static const String similarMoviesEndpoint = "movie_suggestions.json";
+
 }
