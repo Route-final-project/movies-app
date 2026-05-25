731ae1d71435fdfb2a3f0100bf13a859e49b279c
omer1998 <omerfaris11@gmail.com>
2026-05-26 00:14:49 +0300

    Add mainAppScreen and movieDetailScreen routes to RoutesManager

--- lib/core/resource/routes_manager.dart
+++ lib/core/resource/routes_manager.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/cupertino.dart';
+import 'package:movies/domain/entity/movie_entity.dart';
 import 'package:movies/presentation/feature/auth/forget_password_screen.dart';
 import 'package:movies/presentation/feature/auth/login_screen.dart';
 import 'package:movies/presentation/feature/auth/register_screen.dart';
 import 'package:movies/presentation/feature/home/homeScreen.dart';
+import 'package:movies/presentation/mainAppScreen.dart';
+
+import '../../presentation/feature/movie_detail/movieDetailScreen.dart';
 
 abstract class RoutesManger {
   static const String homeScreen = '/homeScreen';
   static const String loginScreen = '/loginScreen';
   static const String registerScreen = '/registerScreen';
   static const String forgetPasswordScreen = '/forgetPasswordScreen';
+  static const String movieDetailScreen = '/movieDetailScreen';
+  static const String mainAppScreenRoute = "/mainAppScreenRoute";
 
   static Map<String, WidgetBuilder> routes = {
     homeScreen: (_) => HomeScreen(),
     loginScreen: (_) => const LoginScreen(),
     registerScreen: (_) => const RegisterScreen(),
     forgetPasswordScreen: (_) => const ForgetPasswordScreen(),
+    mainAppScreenRoute : (_) => const MainAppScreen(),
+    movieDetailScreen: (context) {
+      Map<String, dynamic> argument = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+
+      print("==> movie requested: ${argument["movieEntity"].toString()}");
+      return MovieDetailScreen(movieEntity: argument["movieEntity"]);
+    }
   };
 }
